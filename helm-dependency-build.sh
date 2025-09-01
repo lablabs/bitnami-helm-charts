ab41907825e2e2382edf4b945a2dbcf1197c632e
Stefan Roman <speckle.02stars@icloud.com>
2025-09-01 16:29:21 +0200

    omg

--- helm-dependency-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-for dir in charts/*; do
-  echo "$dir"
-  if yq -e '.dependencies[] | select(.repository | match("https://"))' "$dir/Chart.yaml"; then
-    helm dependency build "$dir" --repository-config "$dir/helm-repositories.yaml"
-  else
-    helm dependency build "$dir" --skip-refresh
-  fi
-done
