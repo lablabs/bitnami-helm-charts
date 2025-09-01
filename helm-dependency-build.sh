8654a5a3cd6a54875683bbf4fb62b31095d32d53
Stefan Roman <speckle.02stars@icloud.com>
2025-09-01 14:47:54 +0200

    trying https and oci

--- helm-dependency-build.sh
+++ helm-dependency-build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 for dir in charts/*; do
-  [ -d "$dir" ] || continue
   if ! yq -e '.dependencies[] | select(.repository | match("^oci://"))' "$dir/Chart.yaml" > /dev/null; then
     helm dependency build "$dir" --skip-refresh --repository-config "$dir/helm-repositories.yaml"
+  else
+    helm dependency build "$dir" --skip-refresh
   fi
 done
