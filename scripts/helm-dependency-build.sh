5d4468629d5e2a58c782601bef247a39f2b7a41e
Stefan Roman <speckle.02stars@icloud.com>
2025-09-01 16:38:01 +0200

    adding repo add

--- scripts/helm-dependency-build.sh
+++ scripts/helm-dependency-build.sh
@@ -3,6 +3,7 @@
 process_chart() {
   dir="$1"
   if yq -e '.dependencies[] | select(.repository | match("https://"))' "$dir/Chart.yaml"; then
+     yq -r '.repositories[] | "helm repo add \(.name) \(.url)"' $dir/helm-repositories.yaml | sh
     helm dependency build "$dir" --repository-config "$dir/helm-repositories.yaml"
   else
     helm dependency build "$dir" --skip-refresh
